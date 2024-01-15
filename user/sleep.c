64a92666e8214f88dfb4a301de0fb2f86198da28
姚树标 <shubiao@staff.weibo.com>
2024-01-15 16:55:40 +0800

    tmp

--- user/sleep.c
+++ user/sleep.c
@@ -2,10 +2,10 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]){
-        if(argc != 2){
-                fprintf(2, "usage: sleep TIME\n");
-                exit(1);
-        }
-        sleep(atoi(argv[1]));
-        exit(0);
+    if(argc != 2){
+        fprintf(2, "usage: sleep TIME\n");
+        exit(1);
+    }
+    sleep(atoi(argv[1]));
+    exit(0);
 }
